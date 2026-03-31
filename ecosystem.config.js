d2dffb5e2ffd7a9688e15761f39e7273401d52fb
Charith Harsha <147936344+Charithharsha08@users.noreply.github.com>
2026-03-31 17:31:49 +0530

    update cloud-sql-auth-proxy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,10 @@
 module.exports = {
     apps: [
-
+        {
+            name: "cloud-sql-auth-proxy",
+            script: "./cloud-sql-proxy project-946811af-a352-4637-bec:asia-southeast1:eca-mysql-instance-n --private-ip",
+            log_file: "./logs/cloud-sql-proxy.log",
+        },
         {
             name: "student-service",
             script: "java -jar student-service/target/Student-Service-1.0.0.jar",
