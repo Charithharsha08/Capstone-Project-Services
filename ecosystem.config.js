ac07264c39eb1ce113cc73210bb188e4da8a1307
Charith Harsha <147936344+Charithharsha08@users.noreply.github.com>
2026-03-31 17:03:47 +0530

    remove cloud-sql-auth-proxy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,6 @@
 module.exports = {
     apps: [
-        {
-          name: "cloud-sql-auth-proxy",
-          script: "./cloud-sql-proxy project-946811af-a352-4637-bec:asia-southeast1:eca-mysql-instance-n project-946811af-a352-4637-bec:us-central1:eca-mysql-instance-new --private-ip",
-          log_file: "./logs/cloud-sql-proxy.log",
-        },
+
         {
             name: "student-service",
             script: "java -jar student-service/target/Student-Service-1.0.0.jar",
