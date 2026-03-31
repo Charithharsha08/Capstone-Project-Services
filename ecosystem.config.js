64d1a3ad7190f34720f7d5e067d660a1a83ec55f
Charith Harsha <147936344+Charithharsha08@users.noreply.github.com>
2026-03-31 17:55:41 +0530

    update cloud-sql-auth-proxy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: "cloud-sql-auth-proxy",
-            script: "./cloud-sql-proxy project-946811af-a352-4637-bec:asia-southeast1:eca-mysql-instance-n --private-ip",
+            script: "./cloud-sql-proxy project-946811af-a352-4637-bec:asia-southeast1:eca-mysql-instance-n project-946811af-a352-4637-bec:us-central1:eca-mysql-instance-new --private-ip",
             log_file: "./logs/cloud-sql-proxy.log",
         },
         {
